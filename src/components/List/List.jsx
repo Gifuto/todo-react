a55179925689e443315cfe86b1556ccb83ff80d2
Bogdan Vasilev <bogdanvasilev@Bogdans-MacBook-Pro.local>
2022-05-09 22:33:07 +0300

    add test remove task

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,25 +6,37 @@ import { Task } from '../Task/Task'
 
 export const List = () => {
     const [tasks, setTasks] = useState([])
-    
+    const id = nanoid()
+
     const addTodo = (text) => {
-        const id = nanoid()
         const newTask = { id, text }
         setTasks((prev) => [...prev, newTask])
+        console.log(id)
     }
 
-    const handleRemoveItem = (e) => {
-        const task = e.target.getAttribute("task")
-        setTasks(tasks.filter(item => item.id !== task));
-       };
+    const handleRemoveItem = id => {
+        // assigning the list to temp variable
+        const temp = [...tasks];
+    
+        // removing the element using splice
+        temp.splice(id, 1);
+    
+        // updating the list
+        setTasks(temp);
+    }
+    
 
     return (
-        <ListWrapper handleRemoveItem = { handleRemoveItem }>
-            <Input addTodo = { addTodo } />
+        <ListWrapper>
+            <Input addTodo = { addTodo }/>
 
             <TasksList>
-                { tasks.map((item) => (
-                    <Task task = { item } key = { item.id } />
+                { tasks.map((item, id) => (
+                    <Task 
+                        task = { item } 
+                        key = { id } 
+                        handleRemoveItem = { handleRemoveItem }
+                    />
                 )) }
             </TasksList>
         </ListWrapper>        
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,13 @@
 import { TaskWrapper, TaskText, DelButton } from './Task.styles'
 
-export const Task = ({ task, handleRemoveItem }) => {
+export const Task = ({ task, handleRemoveItem, id }) => {
     return (
         <TaskWrapper>
             <TaskText>
                 {task.text}
             </TaskText>
 
-            <DelButton onClick={ handleRemoveItem }>
+            <DelButton onClick={() => handleRemoveItem(id)}>
                 X
             </DelButton>
         </TaskWrapper>        
